074f5f4d229afccd167bf8b724cc762076c6b5e8
louiszgm <louiszgm@gmail.com>
2018-11-18 14:47:09 +0800

    server jekyll site

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,6 +1,6 @@
 sudo docker run --rm -p 80:80 -p 443:443 \
      --network nginx_outside \
-     -v /var/jenkins_home/workspace/Nikola/output/:/usr/share/nginx/html \
+     -v /var/jenkins_home/workspace/jekyllBlog/_site/:/usr/share/nginx/html \
      -v $PWD/siteconfs/:/etc/nginx/conf.d \
      -v $PWD/snippets/:/etc/nginx/snippets/ \
      --name mynginx \
