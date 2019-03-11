b185712207804753baa6d4bf94eee963bdf3e3cd
louiszgm <louiszgm@gmail.com>
2019-03-11 11:10:02 +0800

    2019年03月11日 11:10:02

---
+++ ssrmu-node/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+apt-get update && \
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/louiszgm/dockerfiles.git
+docker-compose -f dockerfiles/ssrmu-node/docker-compose.yml up -d
