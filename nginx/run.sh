8ce6c76bbe820655b5fba91effc58a649281ee03
louiszgmCvm <louiszgm@gmail.com>
2019-11-12 15:02:30 +0800

    delete nginx site

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,8 @@
 sudo docker run --rm -p 80:80 -p 443:443 \
      --network nginx_outside \
-     -v /var/jenkins_home/workspace/hugoBlog/public/:/usr/share/nginx/html \
+     -v ~/AppLink/:/usr/share/nginx/html \
      -v $PWD/siteconfs/:/etc/nginx/conf.d \
      -v $PWD/snippets/:/etc/nginx/snippets/ \
+     -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
      --name mynginx \
      -d nginx
